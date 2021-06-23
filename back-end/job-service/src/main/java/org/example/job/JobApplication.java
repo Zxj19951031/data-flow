4cba9785fb4f2258f6c13a348a0206a8b25802e7
zhuxiangjun <zhuxiangjun@duiba.com.cn>
2021-06-23 19:57:41 +0800

    Revert "安全框架"
    This reverts commit 22a73953

--- back-end/job-service/src/main/java/org/example/job/JobApplication.java
+++ back-end/job-service/src/main/java/org/example/job/JobApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author zhuxj
@@ -15,7 +14,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableEurekaClient
 @EnableFeignClients
 @EnableAsync
-@EnableResourceServer
 public class JobApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobApplication.class, args);
